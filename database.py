00996f7191ca2d35a4d2076912b813889e78099a
enjarai <enjarai@pop-os.localdomain>
2020-06-04 11:11:22 +0200

    create database extension

---
+++ database.py
@@ -0,0 +1,92 @@
+import sqlite3
+
+class Database(object):
+    def __init__(self, filename: str):
+        self.connector = sqlite3.connect(filename)
+
+        self.connector.execute("""CREATE TABLE IF NOT EXISTS main.users (
+            id INTEGER PRIMARY KEY UNIQUE NOT NULL,
+            balance INTEGER NOT NULL DEFAULT 0,
+            level INTEGER NOT NULL DEFAULT 0,
+            xp INTEGER NOT NULL DEFAULT 0
+        );""")
+
+        self.connector.commit()
+
+    def close(self):
+        self.connector.close()
+
+    def setup_user(self, userid: int):
+        self.connector.execute("INSERT OR IGNORE INTO main.users (id) VALUES (?);", (str(userid),))
+        self.connector.execute(f"""CREATE TABLE IF NOT EXISTS main.inv_{userid} (
+            id INTEGER PRIMARY KEY UNIQUE NOT NULL,
+            amount INTEGER NOT NULL DEFAULT 0,
+            CHECK (amount >= 0)
+        );""")
+        self.connector.execute(f"""CREATE TABLE IF NOT EXISTS main.eff_{userid} (
+            name TEXT PRIMARY KEY UNIQUE NOT NULL,
+            amount INTEGER NOT NULL DEFAULT 0,
+            CHECK (amount >= 0)
+        );""")
+
+        self.connector.commit()
+
+    def get(self, column: str, userid: int):
+        c = self.connector.execute(f"SELECT {column} FROM main.users WHERE id = ?;", (str(userid),))
+        return c.fetchone()[0]
+
+    def update(self, column: str, userid: int, amount: int):
+        self.connector.execute(f"UPDATE main.users SET {column} = {column} + ? WHERE id = ?;", (str(amount), str(userid)))
+
+        self.connector.commit()
+        return True
+
+    def get_bal(self, *args, **kwargs):
+        return self.get("balance", *args, **kwargs)
+
+    def update_bal(self, *args, **kwargs):
+        return self.update("balance", *args, **kwargs)
+
+    def get_inv(self, userid: int):
+        c = self.connector.execute(f"SELECT * FROM main.inv_{userid}")
+        return c.fetchall()
+
+    def give_item(self, userid: int, itemid: int, amount=1):
+        self.connector.execute(f"INSERT OR IGNORE INTO main.inv_{userid} (id) VALUES (?);", (str(itemid),))
+        self.connector.execute(f"UPDATE main.inv_{userid} SET amount = amount + ? WHERE id = ?;", (str(amount), str(itemid)))
+
+        self.connector.commit()
+        return True
+
+    def rem_item(self, userid: int, itemid: int, amount=1):
+        try:
+            self.connector.execute(f"UPDATE main.inv_{userid} SET amount = amount - ? WHERE id = ?;", (str(amount), str(itemid)))
+        except sqlite3.IntegrityError:
+            return False
+
+        self.connector.execute(f"DELETE FROM main.inv_{userid} WHERE amount = 0;")
+
+        self.connector.commit()
+        return True
+
+    def get_eff(self, userid: int):
+        c = self.connector.execute(f"SELECT * FROM main.eff_{userid}")
+        return c.fetchall()
+
+    def give_eff(self, userid: int, name: str, amount=1):
+        self.connector.execute(f"INSERT OR IGNORE INTO main.eff_{userid} (name) VALUES (?);", (name,))
+        self.connector.execute(f"UPDATE main.eff_{userid} SET amount = amount + ? WHERE name = ?;", (str(amount), str(name)))
+
+        self.connector.commit()
+        return True
+
+    def rem_eff(self, userid: int, name: str, amount=1):
+        try:
+            self.connector.execute(f"UPDATE main.eff_{userid} SET amount = amount - ? WHERE name = ?;", (str(amount), str(name)))
+        except sqlite3.IntegrityError:
+            return False
+            
+        self.connector.execute(f"DELETE FROM main.eff_{userid} WHERE amount = 0;")
+
+        self.connector.commit()
+        return True
